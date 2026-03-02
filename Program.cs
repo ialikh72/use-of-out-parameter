daf4db1e6a1520f39de5ff9c49ff39113b156cb1
ialikh72 <ialikh.w1@gmail.com>
2026-03-02 23:59:52 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int answer,sum,diff;
+        answer=Average(3,4,2,out sum,out diff);
+        Console.WriteLine("Average ="+answer);
+        Console.WriteLine("sum ="+sum);
+        Console.WriteLine("diff ="+diff);
+    }
+    static int Average(int a,int b,int c,out int sum,out int diff)
+    {
+        sum = a + b + c;
+        diff = a - b - c;
+        return sum / 3;
+
+    }
+}
